e1b8ae67a30645d12da89e97809d524386d69b29
Herixth <herixth@gmail.com>
2019-11-28 14:34:55 +0800

    个人信息修改修复:bug:

--- task_platform/urls.py
+++ task_platform/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create-task/', views.create_task, name='create-task'),
-    path('settings/', views.settings, name='settings'),
     path('detail/tk<int:task_id>/', views.detail, name='detail'),
     path('upload/', views.sceneImgUpload, name='uploadimg'),
     path('upload/&responseType=json', views.sceneImgUpload),
--- task_platform/views.py
+++ task_platform/views.py
@@ -48,6 +48,46 @@ def sceneImgUpload(request):
         raise Http404()
 
 
+def settings(request):
+    username = request.session.get('user_name', None)
+    user = User.objects.get(name=username)
+    # POST
+    message = '格式错误'
+    new_password1 = request.POST.get('new_password1', '')
+    new_password2 = request.POST.get('new_password2', '')
+    new_dept = request.POST.get('new_dept', '')
+    new_phone = request.POST.get('new_phone', '')
+    #-------
+    flg_changed = False
+    if new_password1 != new_password2:
+        message = '两次输入的密码不同！'
+        return 
+
+    if new_phone.strip() != '':
+        same_phone = User.objects.filter(phone=new_phone)
+        if same_phone:
+            message = '手机号码已被注册，请重新输入！'
+            return 
+        new_phone = new_phone.strip()
+        if new_phone.isdigit() == False or len(new_phone) != 11:
+            message = '手机号码有误，请重新输入！'
+            return 
+        user.phone = new_phone
+        flg_changed = True
+
+    if new_dept.strip() != '':
+        new_dept = new_dept.strip()
+        if len(new_dept) > 50:
+            message = '学院名过长，请重新输入！'
+            return
+        user.dept = new_dept
+        flg_changed = True
+
+    if flg_changed:
+        user.save()
+        message = ''
+    return
+        
 def index(request):
     # latest_task_list = Task.objects.order_by('-pub_time')[:10]
     username = request.session.get('user_name', None)
@@ -80,42 +120,7 @@ def index(request):
         new_password1, new_password2, new_dept, new_phone
         message
         '''
-        message = '格式错误'
-        new_password1 = request.POST.get('new_password1', '')
-        new_password2 = request.POST.get('new_password2', '')
-        new_dept = request.POST.get('new_dept', '')
-        new_phone = request.POST.get('new_phone', '')
-        #-------
-        flg_changed = False
-        if new_password1 != new_password2:
-            message = '两次输入的密码不同！'
-            return render(request, 'task_platform/index.html', locals())
-
-        if new_phone.strip() != '':
-            same_phone = User.objects.filter(phone=new_phone)
-            if same_phone:
-                message = '手机号码已被注册，请重新输入！'
-                return render(request, 'task_platform/index.html', locals())
-            new_phone = new_phone.strip()
-            if new_phone.isdigit() == False or len(new_phone) != 11:
-                message = '手机号码有误，请重新输入！'
-                return render(request, 'task_platform/index.html', locals())
-            user.phone = new_phone
-            flg_changed = True
-
-        if new_dept.strip() != '':
-            new_dept = new_dept.strip()
-            if len(new_dept) > 50:
-                message = '学院名过长，请重新输入！'
-                return render(request, 'task_platform/index.html', locals())
-            user.dept = new_dept
-            flg_changed = True
-
-        if flg_changed:
-            user.save()
-            message = ''
-            return render(request, 'task_platform/index.html', locals())
-
+        settings(request)
     return render(request, 'task_platform/index.html', locals())
 
 
@@ -131,6 +136,12 @@ def detail(request, task_id):
     username = request.session.get('user_name', None)
     if not username:
         return redirect('/login/')
+    user = User.objects.get(name=username)
+    student_id = user.stu_id
+    phone = user.phone
+    dept = user.dept
+    if user.dept == 'None':
+        dept = '暂无信息'
     
     task = get_object_or_404(Task, pk=task_id)
     tag_list = Task_tags.objects.filter(task_id=task.id)
@@ -151,33 +162,36 @@ def detail(request, task_id):
     if request.method == 'GET':
         accept_id = request.GET.get('accept', None)
         if accept_id:
-            # 单人
             user_task_acc = User_task.objects.get(id=accept_id)
             task_acc = Task.objects.get(id=user_task_acc.task_id)
             task_acc.receiver = user_task_acc.username
             task_acc.task_state = '进行中'
             task_acc.save()
             return redirect('/')
+
     if request.method == 'POST':
-        message = '提交成功'
-        if publisher == username:
-            message = '任务发布者无法提交报价'
-            return render(request, 'task_platform/detail.html', locals())
-        try:
-            temp = User_task.objects.get(task_id=task_id, username=username)
-            message = '您已经提交过报价，请勿重复提交！'
-            return render(request, 'task_platform/detail.html', locals())
-        except:
-            pass
-        
-        description = request.POST.get('description')
-        money = request.POST.get('money')
-        user_task = User_task.objects.create(task_id=task_id)
-        user_task.username = username
-        user_task.description = description
-        # user_task.submit_money = Decimal.from_float(float(money))
-        user_task.submit_money = float(money)
-        user_task.save()
+        if 'settings' in request.POST:
+            settings(request)
+        else:
+            message = '提交成功'
+            if publisher == username:
+                message = '任务发布者无法提交报价'
+                return render(request, 'task_platform/detail.html', locals())
+            try:
+                temp = User_task.objects.get(task_id=task_id, username=username)
+                message = '您已经提交过报价，请勿重复提交！'
+                return render(request, 'task_platform/detail.html', locals())
+            except:
+                pass
+            
+            description = request.POST.get('description')
+            money = request.POST.get('money')
+            user_task = User_task.objects.create(task_id=task_id)
+            user_task.username = username
+            user_task.description = description
+            # user_task.submit_money = Decimal.from_float(float(money))
+            user_task.submit_money = float(money)
+            user_task.save()
     
     return render(request, 'task_platform/detail.html', locals())
 
@@ -191,6 +205,12 @@ def create_task(request):
     username = request.session.get('user_name', None)
     if not username:
         return redirect('/login/')
+    user = User.objects.get(name=username)
+    student_id = user.stu_id
+    phone = user.phone
+    dept = user.dept
+    if user.dept == 'None':
+        dept = '暂无信息'
     tag_list = []
     finder = {
         '未开始': '9', '进行中': '2',
@@ -203,46 +223,49 @@ def create_task(request):
         tag_list.append(
             (task, color,
              Task_tags.objects.filter(task_id=task.id).order_by('sig_tag')))
-             
+
     if request.method == "POST":
-        # 返回该任务详细信息页 /detail/tk Id
-        task_description = request.POST.get('task_description')
-        task_detail = request.POST.get('task_detail')
-        people_needed = request.POST.get('people_needed')
-        expected_time_consuming = request.POST.get('expected_time_consuming')
-        tag_list = request.POST.get('tag_list')
-        tags = tag_list.split(' ')
-        message = '任务创建成功'
-        for tag in tags:
-            if len(tag) > 20:
-                message = '标签：内容过长'
+        if 'settings' in request.POST:
+            settings(request)
+        else:
+            # 返回该任务详细信息页 /detail/tk Id
+            task_description = request.POST.get('task_description')
+            task_detail = request.POST.get('task_detail')
+            people_needed = request.POST.get('people_needed')
+            expected_time_consuming = request.POST.get('expected_time_consuming')
+            tag_list = request.POST.get('tag_list')
+            tags = tag_list.split(' ')
+            message = '任务创建成功'
+            for tag in tags:
+                if len(tag) > 20:
+                    message = '标签：内容过长'
+                    return render(request, 'task_platform/create-task.html', locals())
+            if len(task_description) > 50 or len(task_description) == 0:
+                message = '任务简述：无内容或内容过长！'
                 return render(request, 'task_platform/create-task.html', locals())
-        if len(task_description) > 50 or len(task_description) == 0:
-            message = '任务简述：无内容或内容过长！'
-            return render(request, 'task_platform/create-task.html', locals())
-        if len(task_detail) > 3000 or len(task_detail) == 0:
-            message = '任务详情：无内容或内容过长！'
-            return render(request, 'task_platform/create-task.html', locals())
-        if not people_needed.isdigit():
-            message = '所需人数：请输入数字'
-            return render(request, 'task_platform/create-task.html', locals())
-        if int(people_needed) > 50:
-            message = '所需人数：数字过大'
-            return render(request, 'task_platform/create-task.html', locals())
-        # 没有检查 expected_time_consuming
-        task = Task.objects.create()
-        task.publisher = username
-        task.task_description = task_description
-        task.task_detail = task_detail
-        task.people_needed = people_needed
-        task.expected_time_consuming = Decimal(expected_time_consuming)
-        task.save()
-        for tag in tags:
-            tag_task = Task_tags.objects.create()
-            tag_task.sig_tag = tag
-            tag_task.task_id = task.id
-            tag_task.save()
-        return redirect('/')
+            if len(task_detail) > 3000 or len(task_detail) == 0:
+                message = '任务详情：无内容或内容过长！'
+                return render(request, 'task_platform/create-task.html', locals())
+            if not people_needed.isdigit():
+                message = '所需人数：请输入数字'
+                return render(request, 'task_platform/create-task.html', locals())
+            if int(people_needed) > 50:
+                message = '所需人数：数字过大'
+                return render(request, 'task_platform/create-task.html', locals())
+            # 没有检查 expected_time_consuming
+            task = Task.objects.create()
+            task.publisher = username
+            task.task_description = task_description
+            task.task_detail = task_detail
+            task.people_needed = people_needed
+            task.expected_time_consuming = Decimal(expected_time_consuming)
+            task.save()
+            for tag in tags:
+                tag_task = Task_tags.objects.create()
+                tag_task.sig_tag = tag
+                tag_task.task_id = task.id
+                tag_task.save()
+            return redirect('/')
     return render(request, 'task_platform/create-task.html', locals())
 
 
@@ -256,12 +279,35 @@ def profile(request):
         # 未登录用户无法访问 profile
         return redirect('/login/')
     user = User.objects.get(name=username)
+    student_id = user.stu_id
+    phone = user.phone
+    dept = user.dept
+    if user.dept == 'None':
+        dept = '暂无信息'
     money = user.money   
+    tag_list = []
+    if username:
+        user = User.objects.get(name=username)
+        student_id = user.stu_id
+        phone = user.phone
+        dept = user.dept
+        if user.dept == 'None':
+            dept = '暂无信息'
+
+    finder = {
+        '未开始': '9', '进行中': '2',
+        '中止': '3', '撤销': '3', 
+        '超时': '3', '完成': '4'
+    }
+    latest_task_list = Task.objects.filter(publisher=username).order_by('-pub_time')
+    for task in latest_task_list:
+        color = 'tt-color0{} tt-badge'.format(finder[task.task_state]) 
+        tag_list.append(
+            (task, color,
+             Task_tags.objects.filter(task_id=task.id).order_by('sig_tag')))
     return render(request, 'task_platform/profile.html', locals())
 
 def taskchat(request):
     
     return render(request, 'task_platform/taskchat.html', locals())
 
-def settings(request):
-    return render(request, 'task_platform/page-single_settings.html', locals())
