a444f6ba3cbc21d048ca1cf0107f9461cec0407b
Herixth <herixth@gmail.com>
2019-12-16 08:40:33 +0800

    重建数据库

--- login/views.py
+++ login/views.py
@@ -60,10 +60,10 @@ def send_email(email, code):
 
     html_content = '''
                     <p>感谢注册<a href="http://{}/confirm/?code={}" target=blank>SCU-reward-platform</a></p>
-                    <p>这里是四川大学任务悬赏平台注册系统</p>
+                    <p>这里是Gg四川大学任务悬赏平台注册系统</p>
                     <p>请点击站点链接完成注册确认！</p>
                     <p>此链接有效期为{}天！</p>
-                    '''.format('192.168.1.245:8000', code, settings.CONFIRM_DAYS)
+                    '''.format('127.0.0.1:8000', code, settings.CONFIRM_DAYS)
 
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER, [email])
     msg.attach_alternative(html_content, "text/html")
